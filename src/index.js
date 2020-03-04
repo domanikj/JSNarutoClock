06f469b559428a5560eb6d98b2a06c033503eec2
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-03 19:26:03 -0500

    Added util.js and functions

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import * as util from './Utils';
+
+// Date object
+const date = new Date()
+
+// Date
+let day = date.getDate()
+let month = date.getMonth()
+let year = date.getFullYear()
+
+// Time
+let hour = date.getHours();
+let min  = date.getMinutes();
+let sec  = date.getSeconds();
+
+// Current date & time
+currDate = month + day + year;
+currTime = hour + min + sec;
+
+
