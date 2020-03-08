d0a0dee707d7be66b1dcbb2bfe1cf002891f9ac4
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-07 19:01:05 -0500

    Fixed functions and added time function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import * as util from './Utils';
-
-// Date object
+// Inits
 const date = new Date()
+let grabDate = document.getElementById('date');
 
 // Date
 let day = date.getDate()
@@ -13,13 +12,20 @@ let hour = date.getHours();
 let min  = date.getMinutes();
 let sec  = date.getSeconds();
 
+let currDate = formatDate(padZero(month.toLocaleString()) + padZero(day.toString()) + year.toString());
+
 // Current date & time
-currDate = month + day + year;
-currTime = hour  + min + sec;
+grabDate.innerText = currDate
+console.log(grabDate.textContent)
+let currTime = hour + ':'+ min +':' + sec; 
 
 
+//console.log(grabDate.innerHTML = "ddssdfsdf")
+
+//grabDate.innerHTML = currDate
 // Functions
-
+console.log(grabDate.innerHTML)
+console.log(currTime)
 
 const setBF = () => {
 
@@ -39,3 +45,40 @@ const setStudy = () => {
 const setBreak = () => {
 
 }
+
+
+/*
+* Unshifts 0 at start of string if <= 1
+* @param {String} current month
+* @return an appended 0 at start of string
+*/
+function padZero(str){
+    if(str.length <= 1) return "0"+str;
+}
+
+/*
+* Formats date properly
+* @param {String} current date
+* @return A string with appended '-' 
+*/
+function formatDate(str) {
+    let currDate = ""
+    if(!str.includes('-')){
+       currDate = str.slice(0,2) + '-' +str.slice(2,4) + '-'+str.substr(4);
+       return currDate;
+    }
+}
+
+/*
+* Formats time properly
+* @param {String} current time
+* @return A string with appended ':' 
+*/
+function formatTime(str) {
+    let currTime = ""
+    if(!str.includes('-')){
+       currTime = str.slice(0,2) + '-' +str.slice(2,4) + '-'+str.substr(4);
+       return currTime;
+    }
+}
+
